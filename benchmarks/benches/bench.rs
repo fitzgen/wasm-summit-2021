5ebe0b5b88e3579384744e7e3170100a5da8da2c
Nick Fitzgerald <fitzgen@gmail.com>
2021-04-15 13:47:49 -0700

    blah

--- benchmarks/benches/bench.rs
+++ benchmarks/benches/bench.rs
@@ -1,10 +1,12 @@
 use criterion::*;
 use std::path::Path;
 use std::time::Instant;
+use wasi_common::WasiFile;
 
 fn wasi_engine(strategy: wasmtime::InstanceAllocationStrategy) -> wasmtime::Engine {
     let mut config = wasmtime::Config::new();
 
+    config.cache_config_load_default().unwrap();
     config.allocation_strategy(strategy);
     // config.profiler(wasmtime::ProfilingStrategy::JitDump).unwrap();
     // config.debug_info(true);
@@ -13,13 +15,19 @@ fn wasi_engine(strategy: wasmtime::InstanceAllocationStrategy) -> wasmtime::Engi
     wasmtime::Engine::new(&config).unwrap()
 }
 
-fn make_wasi_ctx(dirs: &[(&Path, &Path)]) -> wasmtime_wasi::WasiCtx {
+fn make_wasi_ctx(
+    stdin: Option<Box<dyn WasiFile>>,
+    dirs: &[(&Path, &Path)],
+) -> wasmtime_wasi::WasiCtx {
     let mut wasi_ctx = wasi_cap_std_sync::WasiCtxBuilder::new()
         // .inherit_stdio()
         .arg("spelling-corrector.wasm")
         .unwrap()
         .arg("funciton")
         .unwrap();
+    if let Some(stdin) = stdin {
+        wasi_ctx = wasi_ctx.stdin(stdin);
+    }
     for (host_dir, guest_dir) in dirs {
         let preopened = unsafe { cap_std::fs::Dir::open_ambient_dir(host_dir).unwrap() };
         wasi_ctx = wasi_ctx.preopened_dir(preopened, guest_dir).unwrap();
@@ -27,14 +35,15 @@ fn make_wasi_ctx(dirs: &[(&Path, &Path)]) -> wasmtime_wasi::WasiCtx {
     wasi_ctx.build().unwrap()
 }
 
-fn wasi_linker(engine: &wasmtime::Engine) -> wasmtime::Linker {
+fn wasi_linker(
+    engine: &wasmtime::Engine,
+    stdin: Option<Box<dyn WasiFile>>,
+    dirs: &[(&Path, &Path)],
+) -> wasmtime::Linker {
     let store = wasmtime::Store::new(&engine);
-    wasmtime_wasi::Wasi::set_context(
-        &store,
-        make_wasi_ctx(&[(Path::new("../spelling-corrector"), Path::new("."))]),
-    )
-    .ok()
-    .unwrap();
+    wasmtime_wasi::Wasi::set_context(&store, make_wasi_ctx(stdin, dirs))
+        .ok()
+        .unwrap();
     wasmtime::Linker::new(&store)
 }
 
@@ -72,7 +81,11 @@ fn spelling_corrector(c: &mut Criterion) {
         let wasm = std::fs::read("../spelling-corrector/control.wasm").unwrap();
         let module = wasmtime::Module::new(&engine, &wasm).unwrap();
         b.iter(|| {
-            let linker = wasi_linker(&engine);
+            let linker = wasi_linker(
+                &engine,
+                None,
+                &[(Path::new("../spelling-corrector"), Path::new("."))],
+            );
             for _ in 0..RUNS {
                 let instance = linker.instantiate(&module).unwrap();
                 call_start(&instance);
@@ -84,7 +97,7 @@ fn spelling_corrector(c: &mut Criterion) {
         let wasm = std::fs::read("../spelling-corrector/wizer.wasm").unwrap();
         let module = wasmtime::Module::new(&engine, &wasm).unwrap();
         b.iter(|| {
-            let linker = wasi_linker(&engine);
+            let linker = wasi_linker(&engine, None, &[]);
             for _ in 0..RUNS {
                 let instance = linker.instantiate(&module).unwrap();
                 call_start(&instance);
@@ -120,7 +133,7 @@ fn instantiate(c: &mut Criterion) {
         b.iter_custom(|iters| {
             let mut elapsed = Default::default();
             for _ in 0..iters {
-                let linker = wasi_linker(&engine);
+                let linker = wasi_linker(&engine, None, &[]);
                 let start = Instant::now();
                 for _ in 0..RUNS {
                     let instance = linker.instantiate(&module).unwrap();
@@ -133,5 +146,76 @@ fn instantiate(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, spelling_corrector, instantiate);
+fn js(c: &mut Criterion) {
+    const RUNS: u64 = 1;
+
+    let mut group = c.benchmark_group("js");
+    group.throughput(Throughput::Elements(RUNS));
+
+    let engine = wasi_engine(if cfg!(feature = "uffd") {
+        wasmtime::InstanceAllocationStrategy::Pooling {
+            strategy: wasmtime::PoolingAllocationStrategy::NextAvailable,
+            module_limits: wasmtime::ModuleLimits {
+                memory_pages: 84,
+                types: 104,
+                ..Default::default()
+            },
+            instance_limits: Default::default(),
+        }
+    } else {
+        wasmtime::InstanceAllocationStrategy::OnDemand
+    });
+
+    group.bench_function("control", |b| {
+        let wasm = std::fs::read("../js-demo/js.wasm").unwrap();
+        let module = wasmtime::Module::new(&engine, &wasm).unwrap();
+        let stdin = include_str!("../../js-demo/dist/main.js");
+        b.iter(|| {
+            let stdin = wasi_common::pipe::ReadPipe::from(stdin);
+            let linker = wasi_linker(&engine, Some(Box::new(stdin) as _), &[]);
+            for _ in 0..RUNS {
+                let instance = linker.instantiate(&module).unwrap();
+                call_start(&instance);
+            }
+        });
+    });
+
+    group.bench_function("wizer", |b| {
+        let wasm = std::fs::read("../js-demo/dist/main.js.wasm").unwrap();
+        let module = wasmtime::Module::new(&engine, &wasm).unwrap();
+        b.iter(|| {
+            let linker = wasi_linker(&engine, None, &[]);
+            for _ in 0..RUNS {
+                let instance = linker.instantiate(&module).unwrap();
+                call_start(&instance);
+            }
+        });
+    });
+
+    group.bench_function("control-instantiate", |b| {
+        let wasm = std::fs::read("../js-demo/js.wasm").unwrap();
+        let module = wasmtime::Module::new(&engine, &wasm).unwrap();
+        b.iter(|| {
+            let linker = wasi_linker(&engine, None, &[]);
+            for _ in 0..RUNS {
+                let instance = linker.instantiate(&module).unwrap();
+                black_box(instance);
+            }
+        });
+    });
+
+    group.bench_function("wizer-instantiate", |b| {
+        let wasm = std::fs::read("../js-demo/dist/main.js.wasm").unwrap();
+        let module = wasmtime::Module::new(&engine, &wasm).unwrap();
+        b.iter(|| {
+            let linker = wasi_linker(&engine, None, &[]);
+            for _ in 0..RUNS {
+                let instance = linker.instantiate(&module).unwrap();
+                black_box(instance);
+            }
+        });
+    });
+}
+
+criterion_group!(benches, spelling_corrector, instantiate, js);
 criterion_main!(benches);
